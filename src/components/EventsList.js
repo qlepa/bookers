dca78c3c60c2fdad4bb77a02d0c73b81588fdfd9
Tomasz Klepacki <t.klepacki@gmail.com>
2019-05-19 22:12:27 +0200

    Added fetching data list view (Work in progress)

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchEvents } from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchEvents } from "../actions";
 
 class EventsList extends React.Component {
-    componentDidMount(){
-        this.props.fetchEvents();
-    }
-    render() {
-        console.log(this.props.events);
-        return <div>Events List</div>;
-    }
-};
+  componentDidMount() {
+    this.props.fetchEvents();
+  }
 
-const mapStateToProps = (state) => {
-    return { events: state.events }
+  renderList() {
+    return this.props.events.map(event => {
+      return (
+        <div className="item" key={event.id}>
+          <div className="content">
+            <h3>{event.eventName}</h3>
+            <p>{event.eventGames[0].outcomes[0].outcomeOdds}</p>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
+  }
 }
 
+const mapStateToProps = state => {
+  return { events: state.events };
+};
+
 export default connect(
-    mapStateToProps, 
-    { fetchEvents }
-    )(EventsList);
+  mapStateToProps,
+  { fetchEvents }
+)(EventsList);
