f64b4c0261c7bb57cbf6f5cf31ded3745ceeee9f
Tomasz Klepacki <t.klepacki@gmail.com>
2019-07-13 09:11:29 +0200

    Updates

---
+++ precache-manifest.7930c4c2a1c05522ce0161e713e64ec3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5b98d02ad059d1e90a67adfd3a0045",
+    "url": "/bookers/index.html"
+  },
+  {
+    "revision": "35644141147776a1b8ff",
+    "url": "/bookers/static/js/2.1455238c.chunk.js"
+  },
+  {
+    "revision": "8b0c691535981f96595f",
+    "url": "/bookers/static/js/main.174cac24.chunk.js"
+  },
+  {
+    "revision": "b3b1e1b73754af9e6f24",
+    "url": "/bookers/static/js/runtime~main.06306efb.js"
+  }
+]);
